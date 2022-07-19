96c19a57eaa3b28199e96bdc6bb44675b5f098a9
Yakup KAYA <iamyakupkaya@outlook.com>
2022-07-19 17:51:43 +0300

    client side --> preloader component added for 2 sec. input and textarea added.

---
+++ client/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <button class="main-app__button">
+    <span>TR</span>
+    <font-awesome-icon icon="fa-solid fa-language" />
+  </button>
+  <PreLoader v-if="showPreloader" />
+  <TodoInput />
+
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+</template>
+
+<script>
+import PreLoader from "./components/PreLoader.vue";
+import TodoInput from "./components/TodoInput.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      showPreloader: true,
+    };
+  },
+  created() {
+    //after 2 seconds, showPreloader will be false so we can not see preloader compo anymore.
+    setTimeout(() => {
+      this.showPreloader = false;
+    }, 2000);
+  },
+
+  components: {
+    PreLoader,
+    TodoInput,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: #e0e0e0;
+}
+.main-app__button {
+  position: fixed;
+  top: 25px;
+  right: 25px;
+  border-radius: 19px;
+  background: linear-gradient(145deg, #f0f0f0, #cacaca);
+  box-shadow: 5px 5px 10px #c7c7c7, -5px -5px 10px #f9f9f9;
+  color: black;
+  font-size: 1.2rem;
+  font-weight: 900;
+}
+.main-app__button span {
+  margin: 10px;
+}
+</style>
---
+++ client/src/components/PreLoader.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="pre-loader">
+    <div class="pre-loader__counter">{{ counter }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    msg: String,
+  },
+  created() {
+    //this will trigger out counter for preloader
+    let interval = setInterval(() => {
+      console.log(this);
+      if (this.counter < 100) {
+        this.counter++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1900 / 100);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../assets/global.css";
+
+.pre-loader {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  column-gap: 20px;
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #070707;
+}
+
+.pre-loader:before {
+  content: "LOADING...";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 10rem;
+  font-weight: 900;
+  color: rgba(255, 255, 255, 0.03);
+}
+
+.pre-loader__counter {
+  position: absolute;
+  top: 75%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #e0e0e0;
+  font-size: 15rem;
+  font-weight: 900;
+  z-index: 1;
+}
+.pre-loader__counter:after {
+  content: "%";
+  font-size: 5rem;
+  font-weight: 900;
+}
+</style>
